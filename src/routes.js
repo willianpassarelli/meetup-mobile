6966b537a31623f7b3c053e8ecba6f903137be63
willianpassarelli <devpassarelli@gmail.com>
2019-10-30 20:18:08 -0300

    signup

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 
 import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 
 export default (isSigned = false) =>
   createAppContainer(
@@ -8,6 +9,7 @@ export default (isSigned = false) =>
       {
         Sign: createSwitchNavigator({
           SignIn,
+          SignUp,
         }),
         // App: createSwitchNavigator({
         //
